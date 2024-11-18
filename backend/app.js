f5fbee8714a129dbde9380b3a057c8440306e763
sixie17 <sakine.yasser@gmail.com>
2024-11-19 00:05:52 +0100

    feat(image): added service and dao

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,14 @@
+require('module-alias/register');
 var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
-require('module-alias/register');
  
 var indexRouter = require("./src/routes/index");
 var usersRouter = require("./src/routes/users");
+var imagesRouter = require("./src/routes/images");
 
 var app = express();
 
@@ -18,13 +19,16 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
 app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+//limiting the size of the request body
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/images", imagesRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
