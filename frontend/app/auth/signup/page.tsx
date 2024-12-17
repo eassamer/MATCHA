3df1fb3c35f9c5e524d34bdb09cbda71500c41ba
sixie17 <sakine@thinkable.co>
2024-12-17 01:55:25 +0100

    fix(mobile): signup

--- frontend/app/auth/signup/page.tsx
+++ frontend/app/auth/signup/page.tsx
@@ -1,9 +1,26 @@
 import { SignUp } from "@/components/auth/Signup/Signup";
+import Link from "next/link";
+
+
+const TermsAndPolicy = () => {
+    return (
+        <div className="h-auto w-fit flex absolute bottom-2">
+            <Link href="/policy/privacy" className="text-pink-500">
+                Terms of use
+            </Link>
+            <div className="w-[50px]"></div>
+            <Link href="/policy/termsofuse" className="text-pink-500">
+                Privacy Policy
+            </Link>
+        </div>
+    );
+}
 
 function Signup() {
     return(
-        <div>
+        <div className="h-screen w-screen flex flex-col items-center">
             <SignUp />
+            <TermsAndPolicy />
         </div>
     )
 }
--- frontend/components/auth/Signup/Signup.tsx
+++ frontend/components/auth/Signup/Signup.tsx
@@ -7,12 +7,12 @@ import { Si42 } from "react-icons/si";
 
 const Media = () => {
     return (
-        <div>
-            <div className="flex items-center justify-center lg:hidden w-full pb-[50px] ">
+        <div className="w-fit h-fit flex flex-col justify-center items-center">
+            <div className="flex items-center justify-center lg:hidden w-full">
                 <Image src="/logo.png" width={276} height={112} alt="Logo" />
             </div>
-            <div className="items-center justify-center flex w-[300px] lg:w-fit">
-                <Image src="/couple.svg" height={215} width={379} alt="" />
+            <div className="items-center justify-center flex md:w-[300px] w-[250px] lg:w-fit">
+                <Image src="/couple.svg" className="md:w-[300px] w-[250px]" height={215} width={300} alt="" />
             </div>
         </div>
     );
@@ -21,14 +21,14 @@ const Media = () => {
 const SignUpOptions = () => {
     const icons = [FaFacebookSquare, FaGoogle, Si42];
     return (
-        <div>
-            <div className="w-full flex flex-col items-center justify-around h-full">
+        <div className="w-full h-fit flex items-center justify-center gap-12 flex-col">
+            <div className="w-full flex flex-col items-center justify-center gap-10 h-fit">
                 <div className="text-black font-montserrat font-bold text-[22px] lg:text-l">
                     Sign up to continue
                 </div>
                 <button className="btn-forward justify-center w-full font-montserrat text-[20px] font-light">Continue with email</button>
             </div>
-            <div className="items-center justify-center w-full h-full">
+            <div className="flex flex-col items-center justify-center w-full h-fit gap-6">
                 <div className="flex flex-row items-center justify-between gap-2 w-full ">
                     <div className="bg-slate-500 h-[1px] w-full"></div>
                     <div className=" text-slate-500 text-center w-full">
@@ -46,26 +46,13 @@ const SignUpOptions = () => {
     );
 }
 
-const TermsAndPolicy = () => {
-    return (
-        <div className="h-auto w-fit flex">
-            <Link href="/policy/privacy" className="text-pink-500">
-                Terms of use
-            </Link>
-            <div className="w-[50px]"></div>
-            <Link href="/policy/termsofuse" className="text-pink-500">
-                Privacy Policy
-            </Link>
-        </div>
-    );
-}
+
 
 export const SignUp = () => {
     return (
-        <div className="flex flex-col lg:flex-row items-center justify-around h-full w-full overflow-hidden">
+        <div className="flex px-12   flex-col lg:flex-row items-center justify-center gap-12 h-full w-full">
             <Media />
             <SignUpOptions />
-            <TermsAndPolicy />
         </div>
     );
 }
