e6f153b833e936dd8da99fef5f865e068e7106f3
Eassamer <68176483+eassamer@users.noreply.github.com>
2024-12-27 21:36:49 +0100

    Feat (frontend): Landing page  (#14)
    * add landing page
    
    * push last changes
    
    * fix respo error
    
    * change react version
    
    * add link routes
    
    * fix:(routers): fixed routers path
    
    ---------
    
    Co-authored-by: Elmehdi Assamer <eassamer@e1r9p11.1337.ma>
    Co-authored-by: sixie17 <sakine.yasser@gmail.com>

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,20 +1,37 @@
+"use server";
+import { AboutUs } from "@/components/LandingPage/AboutUs";
+import { ContactUs } from "@/components/LandingPage/ContactUs";
 import { Header } from "@/components/LandingPage/Header";
 import { Main } from "@/components/LandingPage/Main";
+import { AppSidebar } from "@/components/app-sidebar";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import Image from "@/node_modules/next/image";
+import { SidebarTrigger } from "@/components/ui/sidebar";
+import { Features } from "@/components/LandingPage/Features";
 
-export default function Home() {
+export default async function Home() {
   return (
-    <div className=" w-[100vw] h-[100vh] bg-white px-24 overflow-y-scroll">
-      <div className="w-full h-fit pt-8 pb-6 flex items-center justify-center">
-        <Image src="/logo.png" alt="logo" width={200} height={200} />
-      </div>
-      <Header />
-      <Main />
-      <div className="w-full h-[500px] bg-blue-300 py-8 flex flex-col items-center justify-start gap-12">
-        <h1 className="font-bold font-montserrat tracking-[1.7px] text-[45px] text-black">
-          Our Features
-        </h1>
-      </div>
-    </div>
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <div className=" w-[100vw] h-[100vh] bg-white  overflow-y-scroll">
+          <div className="bg-white shadow lg:shadow-none w-full px-8 mb-2 lg:px-24 h-fit pt-8 pb-6 flex items-center justify-center relative">
+            <SidebarTrigger className="absolute left-12 size-[50px] p-0 lg:hidden -ml-1" />
+            <Image
+              src="/logo.png"
+              alt="logo"
+              width={200}
+              height={200}
+              className="w-[140px] lg:w-[200px]"
+            />
+          </div>
+          <Header />
+          <Main />
+          <Features />
+          <AboutUs />
+          <ContactUs />
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
   );
 }
---
+++ frontend/components/LandingPage/SectionTitle.tsx
@@ -0,0 +1,7 @@
+export const SectionTitle = ({ title }: { title: string }) => {
+  return (
+    <h1 className="font-bold font-montserrat tracking-[1.7px] text-[30px] lg:text-[45px] text-black">
+      {title}
+    </h1>
+  );
+};
