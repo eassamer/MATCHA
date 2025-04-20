7e67f6ccfb8b5aeed5d59070ebcd86268261b333
Eassamer <68176483+eassamer@users.noreply.github.com>
2025-04-20 21:59:10 +0100

    add profile Page and handle edit form (#28)
    * add profile page and fix responsive
    
    * fix sections responsive and change super like style
    
    * add edit dialog
    
    * remove select all in interests field and optimize initialization of interestsOptions

--- frontend/components/profile/profile-about.tsx
+++ frontend/components/profile/profile-about.tsx
@@ -13,13 +13,13 @@ export default function ProfileAbout({ description }: ProfileAboutProps) {
     <div>
       <h2 className="font-bold text-lg mb-2 font-poppins">About</h2>
       <p className="text-gray-700 text-sm lg:text-base font-normal font-poppins">
-        {expanded ? description : description}
+        {expanded ? description : description.slice(0, 100) + "..."}
       </p>
       <button
         onClick={() => setExpanded(!expanded)}
         className="text-primary font-medium text-sm mt-1 font-poppins"
       >
-        Read more
+        {expanded ? "Show less" : "Show more"}
       </button>
     </div>
   );
